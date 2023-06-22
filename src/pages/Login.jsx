5f56ab17da8b88cba4e4f4ac0c2bbe4b3f14975f
Yan Felipe <yanfelipe9@hotmail.com>
2023-06-22 20:25:54 -0300

    ajuste css input login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -50,7 +50,7 @@ function Login() {
                   autoComplete="email"
                   required
                   onChange={(evento) => setEmail(evento.target.value)}
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="block w-full rounded-md border-0 pl-1 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
@@ -71,7 +71,7 @@ function Login() {
                   autoComplete="current-password"
                   required
                   onChange={(evento) => setPasword(evento.target.value)}
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="block w-full rounded-md border-0 pl-1 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
