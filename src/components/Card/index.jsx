eb7c77c8fb4db61119bf051470e9eea63dd0d22e
Yan Felipe <yanfelipe9@hotmail.com>
2023-06-22 20:12:55 -0300

    ajuste do login

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -6,14 +6,19 @@ function Card({ user, children }) {
     <>
       {user ? (
         <div className="flex md:basis-1/4 sm:basis-2/6 basis-3/6 flex-row items-center justify-center bg-slate-300 rounded-2xl">
-            <div className="flex flex-col p-10 items-center ">
-                <img className="h-20 w-20 rounded-full" src={user.gravatarUrl} alt="" />
-                <Link to={`/users/${user.id}`}>
-                    <h1 className="text-lg hover:scale-110 active:scale-100 text-center font-semibold leading-7 tracking-tight text-gray-900">{user.name}</h1>
-                </Link>
-            </div>
+          <div className="flex flex-col p-10 items-center ">
+            <img
+              className="h-20 w-20 rounded-full"
+              src={user.gravatarUrl}
+              alt=""
+            />
+            <Link to={`/users/${user.id}`}>
+              <h1 className="text-lg hover:scale-110 active:scale-100 text-center font-semibold leading-7 tracking-tight text-gray-900">
+                {user.name}
+              </h1>
+            </Link>
+          </div>
         </div>
-        
       ) : (
         <>Sem usuário</>
       )}
@@ -22,4 +27,3 @@ function Card({ user, children }) {
 }
 
 export default Card;
-
