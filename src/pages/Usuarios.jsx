3a5a6996fbee03b8a0e86b2e0edf9f5065948aa9
Yan Felipe <yanfelipe9@hotmail.com>
2023-06-22 23:38:57 -0300

    ajuste do css da pagina dos usuários

--- src/pages/Usuarios.jsx
+++ src/pages/Usuarios.jsx
@@ -30,20 +30,24 @@ export default function ListUsers() {
     <>{Loading()}</>
   ) : (
     <div>
-      <div className="flex justify-center py-3 text-3xl ">
-        <h1>
+      <div className="flex justify-center items-center py-3 text-3xl flex-col font-extralight">
+        <h1 className="text-4xl ">
           Olá{" "}
-          <span className="font-semibold">
+          <span className="font-normal">
             <Link to={`/users/${localStorage.getItem("id")}`}>
               {localStorage.getItem("name")}
             </Link>
           </span>
         </h1>
+        <h2 className="text-2xl">Bem vindo(a) ao <span className="tracking-wider">React Users Link</span></h2>
+        <p className="text-base font-thin">Aqui você encontrará todos os contatos de seus colegas</p>
       </div>
-      <section className="flex flex-wrap justify-center gap-3 py-3">
-        {users.map((user) => (
-          <Card key={user.id} user={user}></Card>
-        ))}
+      <section>
+        <ul className="sm:mx-32 mx-3">
+            {users.map((user) => (
+              <Card key={user.id} user={user}></Card>
+            ))}
+        </ul>
       </section>
     </div>
   );
